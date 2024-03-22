125688c3fdcadecf81465dca8eb83443057b7d84
ArsyadCyber <124241369+ArsyadCyber@users.noreply.github.com>
2024-03-22 09:20:49 +0000

    b

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 module.exports = {
   grupId: '-4101707412',
-  botToken: '7057149683:AAGqzDBcP6-tErCugTuUHgvMFeEEl-JO1gQ'
 };
--- index.js
+++ index.js
@@ -2,21 +2,50 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const path = require('path');
 const cron = require('node-cron');
-const { botToken } = require('./config')
+const express = require('express');
+const bodyParser = require('body-parser');
+const botToken = process.env.BOT_TOKEN;
 
-const scrapeJadwalIndosiar = require('./scrape')
-const bot = new TelegramBot(botToken, { polling: true });
+const scrapeJadwalIndosiar = require('./scrape');
 
+// Inisialisasi dan konfigurasi express
+const app = express();
+const port = process.env.PORT || 3000; // Port yang digunakan oleh server
+
+// Konfigurasi bodyParser
+app.use(bodyParser.json());
+
+// Buat instance bot dengan opsi polling: false karena kita akan menggunakan webhook
+const bot = new TelegramBot(botToken, { polling: false });
+
+// Tentukan URL untuk webhook Anda, ganti 'https://your-webhook-url.com' dengan URL yang diberikan oleh Cyclic.sh
+const webhookUrl = 'https://bb342701-9040-41b5-bde0-f948d4bb808a-00-2kjom1rr02ozu.janeway.replit.dev/';
+
+// Set webhook pada bot dengan URL yang telah ditentukan
+bot.setWebHook(`${webhookUrl}/bot${botToken}`);
+
+// Endpoint untuk menangani pembaruan dari Telegram
+app.post(`/bot${botToken}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Memuat semua command handler dari folder 'commands'
 const commandsPath = path.join(__dirname, 'commands');
 fs.readdirSync(commandsPath).forEach((file) => {
   require(`./commands/${file}`)(bot);
 });
-// Memuat semua command handler dari folder 'commands'
 
+// Tugas terjadwal untuk scraping
 cron.schedule('0 1 * * *', () => {
   console.log('Scraping Indosiar Sedang Dijalankan...');
-    scrapeJadwalIndosiar();
+  scrapeJadwalIndosiar();
 }, {
   scheduled: true,
   timezone: "Asia/Jakarta"
+});
+
+// Jalankan server
+app.listen(port, () => {
+  console.log(`Server berjalan pada port ${port}`);
 });
