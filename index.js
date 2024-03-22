ac9f3bc0f9b7da7b1f616b13b2aa2685a68da368
ArsyadCyber <124241369+ArsyadCyber@users.noreply.github.com>
2024-03-22 09:46:11 +0000

    deploy

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 const bot = new TelegramBot(botToken, { polling: false });
 
 // Tentukan URL untuk webhook Anda, ganti 'https://your-webhook-url.com' dengan URL yang diberikan oleh Cyclic.sh
-const webhookUrl = process.env.WEBHOOK;
+const webhookUrl = process.env.WEBHOOK_URL;
 
 // Set webhook pada bot dengan URL yang telah ditentukan
 bot.setWebHook(`${webhookUrl}/bot${botToken}`);
